106a656b70c7e4803f0764729417ff9679963c26
firdavs <frontendhtml1234@gmail.com>
2023-10-28 17:17:51 +0500

    salom

--- src/components/jadval.vue
+++ src/components/jadval.vue
@@ -1,76 +1,60 @@
 <template>
-    <div>
-        <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">N/0</th>
-      <th scope="col">Heading</th>
-      <th scope="col">Heading</th>
-      <th scope="col">Reaating</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr class="table-primary">
-      <th scope="row">1</th>
-      <td>Cell</td>
-      <td>Cell</td>
-      <td>Cell</td>
-      <td>Cell</td>
-      <td> <n-progress :percentage="70"/></td>
-    </tr>
-    <tr class="table-success">
-      <th scope="row">3</th>
-      <td>Cell</td>
-      <td>Cell</td>
-      <td> <n-progress :percentage="60"/></td>
-    </tr>
-    <tr class="table-danger">
-      <th scope="row">4</th>
-      <td>Cell</td>
-      <td>Cell</td>
-      <td> <n-progress :percentage="50"/></td>
-    </tr>
-    <tr class="table-warning">
-      <th scope="row">5</th>
-      <td>Cell</td>
-      <td>Cell</td>
-      <td> <n-progress :percentage="40"/></td>
-    </tr>
-    <tr class="table-info">
-      <th scope="row">6</th>
-      <td>Cell</td>
-      <td>Cell</td>
-      <td> <n-progress :percentage="30"/></td>
-    </tr>
-    <tr class="table-light">
-      <th scope="row">7</th>
-      <td>Cell</td>
-      <td>Cell</td>
-      <td> <n-progress :percentage="20"/></td>
-    </tr>
-  
-  </tbody>
-</table>
-    </div>
+    <n-scrollbar x-scrollable>
+
+  <div class="white-space: nowrap;">
+    <table class="table" data-mdb-loading="true" borderless: true >
+      <thead>
+        <tr>
+          <th scope="col" class="fw-bold">Name</th>
+          <th scope="col" class="fw-bold">Group</th>
+          <th scope="col" class="fw-bold">Accuracy</th>
+          <th scope="col" class="fw-bold">consistency</th>
+          <th scope="col" class="fw-bold">wpm</th>
+        </tr>
+      </thead>
+      <tbody v-for="srt in data" :key="srt">
+        <tr class="table-primary">
+          <td scope="col" class="fst-normal fw-bold name">{{ srt.last_name }} {{ srt.firt_name  }}</td>
+          <td class="fw-bold">" {{ srt.group }} "</td>
+          <td>{{ srt.accuracy }} %</td>
+          <td>{{ srt.consistency }} %</td>
+
+          <td><n-progress :percentage="srt.wpm" /></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  </n-scrollbar>
 </template>
 
 <script setup>
-import {ref} from 'vue'
-import axios from 'axios'
+import axios from "axios";
+import { ref } from "vue";
 
+let data = ref([]);
 
+function getData() {
+  axios.get('https://djumanov.pythonanywhere.com/results/')
+  .then( resp =>{
+    if (resp.status === 200) 
+     data.value = resp.data
 
+     console.log(data.value = resp.data);
+    
+})
+}
+getData();
 
-
-
-
+// 'https://djumanov.pythonanywhere.com/results/'
 </script>
 
-<style   scoped>
-th:nth-child(1){
-font-size: 1.1rem;
-color: #303F9F;
-font-weight: 700;
-font-family:  sans-serif;
+<style scoped>
+th:nth-child(1) {
+  font-size: 1rem;
+  color: #303f9f;
+  font-family: sans-serif;
+}
+.name{
+  color: #303f9f;
 }
 </style>
